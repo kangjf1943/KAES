5fef4d5972f755e1269f855c0ec84dac6e81d403
Kang <kangjf1943@gmail.com>
2022-05-14 23:00:10 +0200

    feat: 更新汇总计算和导出结果部分代码

--- Es_calc.R
+++ Es_calc.R
@@ -1,6 +1,6 @@
 library(openxlsx)
-library(shapefiles)
 library(readxl)
+library(shapefiles)
 library(dplyr)
 library(ggplot2)
 library(patchwork)
@@ -78,77 +78,78 @@ GetProdCseq <- function(x.prodeff, x.area) {
 }
 
 # 计算2007年和2017年的生产服务和固碳服务
-es.frmlnd.prod.07 <- 
+frmlnd.prod.cseq.07 <- 
   GetProdCseq(prodeff.07, frmlnd.area.07)
-es.frmlnd.prod.17 <- 
+frmlnd.prod.cseq.17 <- 
   GetProdCseq(prodeff.17, frmlnd.area.17)
 
 # N fix ----
 # 思路：
-# 基于各区豆类产量，结合生产绿地旱地和农地旱地总面积的比例，推算各区生产绿地的
+# 将各区豆类产量，乘以生产绿地旱地和农地旱地总面积的比例，推算出各区生产绿地的
 # 豆类产量；再结合文献中的豆科植物单位产量固氮量计算总固氮量
 
 # 读取整理各区农地总面积
 # 函数：读取整合各区农地总面积
-# 输入：原始数据路径及文件名
-fun_allfrmlnd <- function(x) {
-  all_frmlnd <- read_excel(x) %>% 
-    .[c(10:20), ] %>% 
-    # 各区非果树总量按照总农地
-    select(2:8)
-  # 重命名各列列名
-  names(all_frmlnd) <- 
-    c("ward", "total", "self_support_type", 
-      "sale_type", "sale_paddy_field", "sale_dry_land", "sale_orchard")
-  all_frmlnd <- all_frmlnd %>% 
+# 参数：
+# x：原始数据路径及文件名
+GetTotHa <- function(x) {
+  frmlnd.type.area <- read_excel(x)[c(10:20), c(2:8)] %>%  
+    # 重命名各列列名
+    rename_with(~ c("ward", "total", "self_support_type", "sale_type", 
+                  "sale_paddy_field", "sale_dry_land", "sale_orchard")) %>% 
+    # 替换非数字数值
     apply(2, function(x) {gsub("Ｘ", 0, x)}) %>% 
     apply(2, function(x) {gsub("－", 0, x)}) %>%
     apply(2, function(x) {gsub("X", 0, x)}) %>%
-    apply(2, function(x) {gsub("-", 0, x)}) %>%
-    as_tibble() 
-  # 将数据类型均转换为数字类型
-  all_frmlnd[2:7] <- apply(all_frmlnd[2:7], 2, as.numeric) %>% as_tibble()
+    apply(2, function(x) {gsub("-", 0, x)}) %>% 
+    as_tibble()
   
-  all_frmlnd <- all_frmlnd %>% 
+  # 将数据类型均转换为数字类型
+  frmlnd.type.area[2:7] <- apply(frmlnd.type.area[2:7], 2, as.numeric) %>% as_tibble()
+  
+  frmlnd.type.area <- frmlnd.type.area %>% 
     # 重构总面积为自给型和贩卖型之和
     mutate(total = self_support_type + sale_type) %>% 
     # 各区旱地面积计算：
     # 假设总面积中旱地占比和贩卖型农田中旱地占比相同
-    mutate(total_dry_land = total * sale_dry_land / sale_type)
+    mutate(tot_ha_area = total * sale_dry_land / sale_type) %>% 
+    select(ward, tot_ha_area)
+  
   # 替换计算结果中的NaN
-  all_frmlnd[is.na(all_frmlnd)] <- 0
+  frmlnd.type.area[is.na(frmlnd.type.area)] <- 0
+  
   # 返回结果
-  return(all_frmlnd)
+  return(frmlnd.type.area)
 }
 
-# 读取并整理2007年的数据，单位为公顷
-all_frmlnd_07 <- fun_allfrmlnd("RRawData/Kyoto_all_farmland_area_2007.xls")
-# 将单位转化为平方米
-all_frmlnd_07[2:8] <- apply(all_frmlnd_07[2:8], 2, function(x) {x*10000})
+# 计算全市旱地面积
+# 计算2007年的全市旱地面积，单位为公顷
+tot.ward.ha.area.07 <- GetTotHa("RRawData/Kyoto_all_farmland_area_2007.xls") %>% 
+  # 将单位转化为平方米
+  mutate(tot_ha_area = tot_ha_area * 10000)
 
-# 读取并整理2007年的数据，单位为公顷
-all_frmlnd_17 <- fun_allfrmlnd("RRawData/Kyoto_all_farmland_area_2017.xlsx")
-# 将单位转化为平方米
-all_frmlnd_17[2:8] <- apply(all_frmlnd_17[2:8], 2, function(x) {x*100})
-# 数据初步分析结果：
-# 贩卖型为主；贩卖型中又以水田为主
-# 大部分行政区2017年农地总面积相比2007年减少
-# 问题：但是右京区反而增加了？
+# 计算2017年的全市旱地面积，单位为100平方米
+tot.ward.ha.area.17 <- GetTotHa("RRawData/Kyoto_all_farmland_area_2017.xlsx") %>% 
+  # 将单位转化为平方米
+  mutate(tot_ha_area = tot_ha_area * 100)
 
 # 汇总计算2007年和2017年各区生产绿地旱地面积
-es_dry_land_07 <- frmlnd.area.07 %>% 
+ward.ha.area.07 <- frmlnd.area.07 %>% 
   subset(type == "ha") %>%
-  group_by(city_name) %>% 
+  group_by(ward) %>% 
   summarise(area = sum(area))
 
-es_dry_land_17 <- frmlnd.area.17 %>% 
+ward.ha.area.17 <- frmlnd.area.17 %>% 
   subset(type == "ha") %>%
-  group_by(city_name) %>% 
+  group_by(ward) %>% 
   summarise(area = sum(area))
 
-# 读取豆科作物产量
-all_legume_nfix <- 
+# 计算全市固氮量
+tot.ward.nfix <- 
+  # 读取豆科作物产量
   read_xlsx("RRawData/Legume_area_production.xlsx", sheet = "LegumeData") %>% 
+  rename(yield = yield_t) %>% 
+  select(species, ward, area_ha, yield) %>% 
   # 基于文献提取各种豆科植物单位面积固氮量
   # 采用Herridge et al. 2008文献中
   # Table 5的Calculated from Table 4 (kg N/ha/year)列的数据
@@ -163,75 +164,116 @@ all_legume_nfix <-
     species == "さやいんげん" ~ 23
   )) %>% 
   # 整理并转化area_ha列数据类型
-  mutate(area_ha = as.numeric(gsub("-", 0, area_ha)))
-# 替换area_ha为NA的值
-all_legume_nfix$area_ha[is.na(all_legume_nfix$area_ha)] <- 0
-all_legume_nfix <- all_legume_nfix %>% 
-  # 计算固氮量，单位为kg N
+  mutate(area_ha = as.numeric(gsub("-", 0, area_ha))) %>% 
+  # 替换旱地面积和产量为NA的值
+  mutate(area_ha = ifelse(is.na(area_ha), 0, area_ha), 
+         yield = ifelse(is.na(yield), 0, yield)) %>% 
+  # 计算固氮量，单位为：千克N
   mutate(nfix = nfix_rate * area_ha) %>% 
   # 汇总计算各区总量
   group_by(ward) %>% 
   summarise(nfix = sum(nfix)) %>% 
   ungroup()
 
-# 合并豆科总固氮量和2007和2017年各区推算旱地总面积数据和生产绿地旱地的面积
+# 推算生产绿地固氮量
 # 函数：计算固氮量
-GetNFix <- function(all_frmlnd, es_dry_land) {
-  es_nfix <- all_frmlnd[c("ward", "total_dry_land")] %>% 
-    left_join(es_dry_land, by = c("ward" = "city_name")) %>% 
+# 参数：
+# tot.ward.ha.area：全市口径各区旱地面积
+# ward.ha.area：生产绿地口径各区旱地面积
+GetNFix <- function(tot.ward.ha.area, ward.ha.area) {
+  ward.nfix <- tot.ward.ha.area[c("ward", "tot_ha_area")] %>% 
+    left_join(ward.ha.area, by = "ward") %>% 
     # 计算生产绿地旱地和总体旱地面积的比例
-    mutate(rate = area / total_dry_land) %>% 
-    left_join(all_legume_nfix, by = "ward") %>% 
-    # 计算2007年和2017年生产绿地的固氮量
-    mutate(nfix = nfix * rate)
+    mutate(rate = area / tot_ha_area) %>% 
+    left_join(tot.ward.nfix, by = "ward") %>% 
+    # 推算生产绿地的固氮量
+    mutate(nfix = nfix * rate) %>% 
+    # 替换NaN值
+    mutate(nfix = ifelse(is.na(nfix), 0, nfix)) %>% 
+    select(ward, nfix)
   
-  # 替换NaN值
-  es_nfix$nfix[is.nan(es_nfix$nfix)] <- 0
-  
-  return(es_nfix)
+  return(ward.nfix)
 }
+
 # 并且推算生产绿地的固氮量
-es_nfix_07 <- GetNFix(all_frmlnd_07, es_dry_land_07)
-es_nfix_17 <- GetNFix(all_frmlnd_17, es_dry_land_17)
+ward.nfix.07 <- GetNFix(tot.ward.ha.area.07, ward.ha.area.07)
+ward.nfix.17 <- GetNFix(tot.ward.ha.area.17, ward.ha.area.17)
 
 # Cooling effect ---- 
-es.frmlnd.cool.07 <- 
+frmlnd.cool.07 <- 
   read.shapefile("GProcData/Kyoto_prod_green_space_cool_2007") %>% 
   .$dbf %>% .$dbf %>% as_tibble() %>% 
   rename_with(tolower) %>% 
-  select(plotid, cool_eff)
+  rename(cool = cool_eff) %>% 
+  select(plotid, cool)
 
-es.frmlnd.cool.17 <- 
+frmlnd.cool.17 <- 
   read.shapefile("GProcData/Kyoto_prod_green_space_cool_2017") %>% 
   .$dbf %>% .$dbf %>% as_tibble() %>% 
   rename_with(tolower) %>% 
-  select(plotid, cool_eff)
+  rename(cool = cool_eff) %>% 
+  select(plotid, cool)
 
 # Sum to ward level ----
 # 计算各区生态系统服务量
-SumEs <- function(es.prod.cseq, es.cool, es.nfix) {
-  es_ward <- es.prod.cseq %>%
-    left_join(es.cool, by = "plotid") %>% 
-    group_by(city_name) %>% 
-    summarise(
-      area = sum(area), 
+SumEs <- function(frmlnd.prod.cseq, frmlnd.cool, ward.nfix) {
+  es.ward <- frmlnd.prod.cseq %>% 
+    select(plotid, ward, rice, veg, cseq) %>% 
+    left_join(frmlnd.cool %>% select(plotid, cool), by = "plotid") %>% 
+    group_by(ward) %>% 
+    summarise( 
       rice = sum(rice), 
       veg = sum(veg), 
       cseq = sum(cseq), 
-      cool = sum(cool_eff)) %>% 
+      cool = sum(cool)) %>% 
     ungroup() %>% 
     # 合并固氮量结果
-    left_join(es.nfix[c("ward", "nfix")], by = c("city_name" = "ward"))
-  return(es_ward)
+    left_join(ward.nfix[c("ward", "nfix")], by = "ward")
+  
+  return(es.ward)
 }
 
-es_ward_07 <- SumEs(es.frmlnd.prod.07, es.frmlnd.cool.07, es_nfix_07)
-es_ward_17 <- SumEs(es.frmlnd.prod.17, es.frmlnd.cool.17, es_nfix_17)
+ward.es.07 <- SumEs(frmlnd.prod.cseq.07, frmlnd.cool.07, ward.nfix.07)
+ward.es.17 <- SumEs(frmlnd.prod.cseq.17, frmlnd.cool.17, ward.nfix.17)
 
-# bug：此处应该补一个将名字重命名为驼峰式写法之后再输出的函数
-write.csv(es_ward_07, "/Users/Kang/Documents/R/KAES/GProcData/Es_ward_07.csv")
-write.csv(es_ward_07, "/Users/Kang/Documents/R/KAES/GProcData/Es_ward_17.csv")
+# Export MS Excel ----
+# 函数：将通过下划线连接的字符串改成驼峰式写法
+# 参数：
+# x：字符串
+TurnCamel <- function(x) {
+  # 根据下划线拆分字符串
+  strsplit(x, "_")[[1]] %>% 
+    # 将每个元素都改成首字母大写
+    Hmisc::capitalize() %>% 
+    # 将新的字符串拼接起来
+    Reduce(paste0, .) %>% 
+    return()
+}
 
+# 函数：将数据框列名重命名为驼峰式写法，再输出为*.csv文件
+# 参数：
+# ward.es：待写出的数据框
+# file.name：输出路径和文件名
+WriteCamelCsv <- function(ward.es, file.name) {
+  for (i in names(ward.es)) {
+    names(ward.es)[names(ward.es) == i] <- TurnCamel(i)
+  }
+  write.csv(ward.es, file.name)
+}
+
+# 导出各地块生态系统服务
+# 注意，固氮服务只有区粒度的结果，而无地块粒度的结果，因为不知道哪些地块种的是豆
+# 科植物，因此无法将结果分配到地块
+frmlnd.prod.cseq.07 %>% left_join(frmlnd.cool.07, by = "plotid") %>% 
+  WriteCamelCsv(file.name = "GProcData/Frmlnd_es_07.csv")
+frmlnd.prod.cseq.17 %>% left_join(frmlnd.cool.17, by = "plotid") %>% 
+  WriteCamelCsv(file.name = "GProcData/Frmlnd_es_17.csv")
+
+# 导出各区生态系统服务
+WriteCamelCsv(ward.es = ward.es.07, file.name = "GProcData/Ward_es_07.csv")
+WriteCamelCsv(ward.es = ward.es.17, file.name = "GProcData/Ward_es_17.csv")
+
+# Visualization ----
 # 分析各区ES差异
 plot_es_ward <- vector("list", length = ncol(es_ward) - 1)
 names(plot_es_ward) <- names(es_ward)[-1]
